422691983c9167fefc4c221be3c888fc2d7f1fa6
IsuruFerna <isurumadushan.ferna@gmail.com>
2024-01-29 17:20:42 +0100

    bug fix statoFatturaRepo

--- src/main/java/epicenergy_backend_buildweek/team5_buildweek2_backend/repositories/StatoFatturaRepository.java
+++ src/main/java/epicenergy_backend_buildweek/team5_buildweek2_backend/repositories/StatoFatturaRepository.java
@@ -1,8 +1,9 @@
 package epicenergy_backend_buildweek.team5_buildweek2_backend.repositories;
 
+import epicenergy_backend_buildweek.team5_buildweek2_backend.entities.StatoFattura;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface StatoFatturaRepository extends JpaRepository<StatoFatturaRepository,String> {
+public interface StatoFatturaRepository extends JpaRepository<StatoFattura, String> {
 }
