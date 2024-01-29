c9d0f08883c6ff313e52a0eb21e6723e2d900bbb
federicopignatelli <federicopignatelli@icloud.com>
2024-01-29 12:59:55 +0100

    fix

---
+++ src/main/java/epicenergy_backend_buildweek/team5_buildweek2_backend/payloads/users/ErrorsDTO.java
@@ -0,0 +1,6 @@
+package epicenergy_backend_buildweek.team5_buildweek2_backend.payloads.users;
+
+import java.time.LocalDateTime;
+
+public record ErrorsDTO(String message, LocalDateTime timestamp) {
+}
