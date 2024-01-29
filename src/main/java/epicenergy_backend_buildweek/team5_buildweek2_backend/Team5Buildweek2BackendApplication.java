09e2f78049c13a43b14721638a06999d23ed739e
federicopignatelli <federicopignatelli@icloud.com>
2024-01-29 11:41:57 +0100

    Initial commit

---
+++ src/main/java/epicenergy_backend_buildweek/team5_buildweek2_backend/Team5Buildweek2BackendApplication.java
@@ -0,0 +1,13 @@
+package epicenergy_backend_buildweek.team5_buildweek2_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Team5Buildweek2BackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Team5Buildweek2BackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/epicenergy_backend_buildweek/team5_buildweek2_backend/Team5Buildweek2BackendApplicationTests.java
@@ -0,0 +1,13 @@
+package epicenergy_backend_buildweek.team5_buildweek2_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Team5Buildweek2BackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
