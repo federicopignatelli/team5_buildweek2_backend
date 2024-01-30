01f53dc05b029695766498cbad0978f7e53a7fb7
IsuruFerna <isurumadushan.ferna@gmail.com>
2024-01-30 09:51:45 +0100

    prova

---
+++ epic-energy/src/App.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+import "./App.css";
+import "bootstrap/dist/css/bootstrap.min.css";
+import { LoginPage } from "./pages/LoginPage";
+
+function App() {
+   return <LoginPage />;
+}
+
+export default App;
